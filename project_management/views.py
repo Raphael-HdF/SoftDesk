fe472914bf8f43c692255ed2d6c38346e45f3a7a
Raphael-HdF <rdefrob@gmail.com>
2022-09-28 10:56:30 +0200

    [IMP] Improving handling of the users

--- project_management/views.py
+++ project_management/views.py
@@ -1,3 +1,6 @@
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Project, Contributor, Issue, Comment
@@ -29,9 +32,20 @@ class ContributorViewSet(ModelViewSet):
     permission_classes = [IsContributor]
 
     def get_queryset(self):
-        return Contributor.objects.filter(
+        contributor = Contributor.objects.filter(
             project=self.kwargs['project_pk']
         )
+        if self.kwargs.get('pk'):
+            contributor = contributor.filter(
+                user=self.kwargs['pk'],
+            )
+        return contributor
+
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+        obj = get_object_or_404(queryset)
+        self.check_object_permissions(self.request, obj)
+        return obj
 
 
 class ProjectViewSet(ModelViewSet):
